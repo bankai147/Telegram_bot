84316e58f17b4bea5f406c54648f73e26b639fc3
bankai147 <shadowinvisible22@gmail.com>
2025-04-14 23:24:23 +0200

    Add files via upload

---
+++ Bot1.py
@@ -0,0 +1,15 @@
+import telebot
+bot = telebot.TeleBot('8144284032:AAH6SIkPcnm3v5ZkU28-3XEFOe_d52RKjcI')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Hello! I am a bot that can help you with various tasks. Type /help to see what I can do!')
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, 'Here are the commands you can use:\n/start - Start the bot\n/help - Get help\n/echo - Echo back your message')
+@bot.message_handler(commands=['echo'])
+def echo(message):
+    bot.send_message(message.chat.id, 'Please send me a message to echo back.')
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    bot.send_message(message.chat.id, 'You said: ' + message.text)
+    bot.polling(none_stop=True)
